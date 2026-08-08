3d7594151e390c4f6143bc0f1ca412bd34fb2ed9
TristanAndreievitchLandry <tristanlandry@protonmail.com>
2026-08-08 02:52:17 -0400

    switchIcons

--- main.js
+++ main.js
@@ -78,9 +78,9 @@ function ensureAppShellElements() {
       'beforeend',
       '<div class="button-container">' +
         '<button id="info-button" title="" data-i18n-attr="title:buttons.info"><i class="fa-solid fa-info"></i></button>' +
-        '<button id="save-button" onclick="downloadDrawnLayers()" title="" data-i18n-attr="title:buttons.save"><i class="fa-solid fa-download"></i></button>' +
         '<button id="ask-button" style="font-size: 20px" title="" data-i18n-attr="title:buttons.manifest"><i class="fa-solid fa-image"></i></button>' +
         IIIF_GUIDE_BUTTON_HTML +
+        '<button id="save-button" onclick="downloadDrawnLayers()" title="" data-i18n-attr="title:buttons.save"><i class="fa-solid fa-download"></i></button>' +
         '<button id="add-button" title="" data-i18n-attr="title:buttons.importJson"><i class="fa-solid fa-plus"></i></button>' +
         '<button id="canvas-prev" title="" data-i18n-attr="title:buttons.previousPage"><i class="fa-solid fa-arrow-left"></i></button>' +
         '<button id="canvas-next" title="" data-i18n-attr="title:buttons.nextPage"><i class="fa-solid fa-arrow-right"></i></button>' +
